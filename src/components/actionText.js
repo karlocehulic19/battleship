d01f40f0cd88030c650cb3e02615f78d990182a0
Karlo Cehulic <karlocehlic@gmail.com>
2025-08-31 00:17:51 +0200

    Add text action game state logic

--- src/components/actionText.js
+++ src/components/actionText.js
@@ -1,9 +1,22 @@
+import { GameState, globalGameState } from "../logic";
+
 export function createActionText() {
+  const messageMapping = {
+    [GameState.states["PLAYER_TURN"]]:
+      "Its your turn, click on oppents board to attack his shitps",
+    [GameState.states["COMPUTER_TURN"]]:
+      "Wait for the opponent to finsih attacking",
+    [GameState.states["PLACING_SHIPS"]]:
+      "Hello, place your boards and click play to start!",
+  };
+
   const actionText = document.createElement("h1");
   actionText.id = "action-text";
 
-  actionText.textContent = "Hello, place your boards and click play to start!";
+  actionText.textContent = messageMapping[GameState.states["PLACING_SHIPS"]];
   document.body.appendChild(actionText);
-}
 
-addEventListener(type, listener);
+  addEventListener(globalGameState.stateChangeEvent, () => {
+    actionText.textContent = messageMapping[globalGameState.state];
+  });
+}
